8725e505e8b4088dc637122592c922311d0f16a8
gildaciozz <gildacioj399@gmail.com>
2025-02-23 17:50:32 -0300

    feat: ensure order can be created

--- src/app/[slug]/menu/helpers/cpf.ts
+++ src/app/[slug]/menu/helpers/cpf.ts
@@ -1,3 +1,8 @@
+export const removeCpfPunctuation = (cpf: string) => {
+  return cpf.replace(/[\.\-]/g, "");
+};
+
+
 export const isValidCpf = (cpf: string): boolean => {
     // Remove caracteres não numéricos
     cpf = cpf.replace(/\D/g, "");
