37400f280082c4dbb7a2ee9effdac6ba827e3371
Héctor Oviedo <nosis_r8@hotmail.com>
2026-01-31 14:47:17 -0300

    feat: update environment configuration, enhance package dependencies, and adjust TypeScript settings

--- src/app/[slug]/menu/actions/create-stripe-checkout.ts
+++ src/app/[slug]/menu/actions/create-stripe-checkout.ts
@@ -42,7 +42,7 @@ export const createStripeCheckout = async ({
     },
   });
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-    apiVersion: "2025-02-24.acacia",
+    apiVersion: "2026-01-28.clover",
   });
   const searchParams = new URLSearchParams();
   searchParams.set("consumptionMethod", consumptionMethod);
--- src/app/api/webhooks/stripe/route.ts
+++ src/app/api/webhooks/stripe/route.ts
@@ -9,7 +9,7 @@ export async function POST(request: Request) {
     throw new Error("Missing Stripe secret key");
   }
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-    apiVersion: "2025-02-24.acacia",
+    apiVersion: "2026-01-28.clover",
   });
 
   const signature = request.headers.get("stripe-signature");
